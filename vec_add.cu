a9aa72b392bb5443f0d761b0958e37dda3489496
Vish Chaudhary <97370632+VishChaudhary@users.noreply.github.com>
2024-03-10 17:21:45 -0400

    Update vec_add.cu

--- vec_add.cu
+++ vec_add.cu
@@ -1,56 +1,64 @@
 #include "stdio.h"
 #include "math.h"
 
-__device__ void vecAdd(double* a,double *b, double *c, int n){
+__global__ void vecAdd(float* a,float *b, float *c, int n){
   int i = blockDim.x * blockIdx.x + threadIdx.x;
   if(i<n){
     c[i] = a[i] + b[i];
   }
-}
-
-__global__ void createMat(double* a,double *b,double*c, int n){
-  int i = blockIdx.x * blockDim.x + threadIdx.x;
-  if(i<n){
-    a[i] = sin(i) + cos(i);
-    b[i] = cos(i)*sin(i);
+  if(i==50){
+    printf("GPU working, i=50\n");
   }
-  vecAdd<<<ceil(n/256.0), 256>>>(a,b,c,n);
 }
 
+// __global__ void createMat(float* a,float *b,float*c, int n){
+//   int i = blockIdx.x * blockDim.x + threadIdx.x;
+//   if(i<n){
+//     a[i] = sin(i) + cos(i);
+//     b[i] = cos(i)*sin(i);
+//   }
+//   //vecAdd<<<ceil(n/256.0), 256>>>(a,b,c,n);
+// }
 
 
 int main(){
 
-int n = 10000;
+  int n = 10000;
 
-int bytes = n * sizeof(double);
+  size_t bytes = n * sizeof(float);
 
-double *a_h = (double*)malloc(bytes);
-double *b_h = (double*)malloc(bytes);
-double *c_h = (double*)malloc(bytes);
+  float *a_h = (float*)malloc(bytes);
+  float *b_h = (float*)malloc(bytes);
+  float *c_h = (float*)malloc(bytes);
 
-double *a_d, b_d, c_d;
+  float *a_d, *b_d, *c_d;
 
-cudaMalloc(&a_d, bytes);
-cudaMalloc(&b_d, bytes);
-cudaMalloc(&c_d, bytes);
+  cudaMalloc(&a_d, bytes);
+  cudaMalloc(&b_d, bytes);
+  cudaMalloc(&c_d, bytes);
 
-cudaMemcpy(a_d, a_h,bytes, cudaMemcpyHostToDevice);
-cudaMemcpy(b_d, b_h,bytes, cudaMemcpyHostToDevice);
+  for(int i=0; i<n; i++){
+    a_h[i] = sin(i) + cos(i);
+    b_h[i] = cos(i)*sin(i);
+  }
 
-createMat<<<ceil(n/256.0), 256>>>(a_d,b_d,c_d, n);
+  cudaMemcpy(a_d, a_h,bytes, cudaMemcpyHostToDevice);
+  cudaMemcpy(b_d, b_h,bytes, cudaMemcpyHostToDevice);
 
-cudaMemcpy(c_h,c_d, bytes, cudaMemcpyDeviceToHost);
+  //createMat<<<ceil(n/256.0), 256>>>(a_d,b_d,c_d, n);
+  vecAdd<<<ceil(n/256.0), 256>>>(a_d,b_d,c_d,n);
+  cudaMemcpy(c_h,c_d, bytes, cudaMemcpyDeviceToHost);
+  printf("%f",c_h[5]);
 
-free(a_h);
-free(b_h);
-free(c_h);
+  free(a_h);
+  free(b_h);
+  free(c_h);
 
-cudaFree(a_d);
-cudaFree(b_d);
-cudaFree(c_d);
+  cudaFree(a_d);
+  cudaFree(b_d);
+  cudaFree(c_d);
 
-return 0;
+  return 0;
 }
 
 
